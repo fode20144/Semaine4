4c7963e3774f269cd88707db0e7e7263e713350c
fode20144 <sanogofode@gmail.com>
2015-10-20 22:13:59 +0000

    Ajout code appli

--- openfactfood.rb
+++ openfactfood.rb
@@ -1,2 +1,12 @@
 require 'open-uri'
 require 'json'
+loop do
+puts "Sur quel produit vous desirez travaillez ? "
+puts "Code barre : "
+cde_barr = gets.chomp
+url =  "http://fr.openfoodfacts.org/api/v0/produit/#{cde_barr}.json"
+json = open(url).read
+data = JSON.parse(json)
+puts data['product']['product_name']
+
+end
